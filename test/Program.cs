346f1ea167aa263e5115b36a3fdd100c30b5feed
Dimitar Todorov <36155679+batgo6o@users.noreply.github.com>
2020-02-18 14:49:29 +0200

    Add files via upload

--- test/Program.cs
+++ test/Program.cs
@@ -11,30 +11,20 @@ namespace test
         static void Main(string[] args)
         {
             
-            int[] numbers = { 5, 4, 3, 9, 2 };
-            int min = numbers[0];
-            for (int i = 1; i < numbers.Length ; i++)
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 1; i < numbers.Length; i++)
             {
-                if (numbers[i] > numbers[i - 1])
+                if (numbers[i - 1] > numbers[i])
                 {
-                    int reserve = numbers[i + 1];
-                    numbers[i + 1] = numbers[i];
-                    numbers[i] = reserve;
-                }
-                if (numbers[i] < numbers[i - 1])
-                {
-                    int reserve = numbers[i - 1];
-                    numbers[i - 1] = numbers[i];
-                    numbers[i] = reserve;
-                }
-                if (numbers[i] < min)
-                {
-                    int reserve = numbers[0];
-                    numbers[0] = numbers[i];
-                    numbers[i] = reserve;
+                    int reserve = numbers[i];
+                    numbers[i] = numbers[i - 1];
+                    numbers[i - 1] = reserve;
+                    i = 0;
                 }
             }
             Console.WriteLine(string.Join(" ", numbers));
         }
+                    
     }
 }
