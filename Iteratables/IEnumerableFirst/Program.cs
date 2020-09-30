23de4e4a42ad719e6b1f337a038d74a1adfc2dd0
Dimitar Todorov <36155679+batgo6o@users.noreply.github.com>
2020-10-01 01:06:23 +0300

    First steps with Iteratables

---
+++ Iteratables/IEnumerableFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IEnumerableFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
